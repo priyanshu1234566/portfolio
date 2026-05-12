9624b729d3bec854369f94dcd54b35e84130eb7e
priyanshu1234566 <priyanshukumar2004panipat@gmail.com>
2026-05-12 22:20:25 +0530

    up098

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -158,7 +158,7 @@ export default function Navbar() {
             </h2>
 
             <p className="text-sm text-cyan-400">
-              WEB Developer
+      
             </p>
 
           </div>
